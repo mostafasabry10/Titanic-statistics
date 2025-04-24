2b2a1a9cd60f6b472e38f66509618b41e1f60198
mostafasabry10 <mostafasabry916@gmail.com>
2025-04-25 01:26:32 +0300

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd 
 import plotly.express as px 
 import streamlit as st
-import os 
-os.chdir(r'C:/Users/mostafa/the new project)
+
 st.set_page_config(layout="wide", page_title = 'Simple DashBoard')
 
 html_title = """<h1 style="color:red;text-align:center;"> Titanic statistics and plots </h1>"""
